0c5b9b1efd0b60d7bf13d05a85c5f51407610559
Carter Borchetta <carterborch@me.com>
2020-07-05 11:00:02 -0400

    Outlets for LaunchDetail

---
+++ Launchpad/LaunchDetailViewController.swift
@@ -0,0 +1,52 @@
+//
+//  LaunchDetailViewController.swift
+//  Launchpad
+//
+//  Created by Carter Borchetta on 7/5/20.
+//  Copyright © 2020 Carter Borchetta. All rights reserved.
+//
+
+import UIKit
+
+class LaunchDetailViewController: UIViewController {
+    
+    
+    @IBOutlet weak var detailCountDownLabel: UILabel!
+    @IBOutlet weak var vehicleLabel: UILabel!
+    @IBOutlet weak var blockLabel: UILabel!
+    @IBOutlet weak var serialLabel: UILabel!
+    @IBOutlet weak var flightNumberLabel: UILabel!
+    @IBOutlet weak var reusedLabel: UILabel!
+    @IBOutlet weak var customerLabel: UILabel!
+    @IBOutlet weak var countryLabel: UILabel!
+    @IBOutlet weak var massLabel: UILabel!
+    @IBOutlet weak var orbitLabel: UILabel!
+    @IBOutlet weak var payloadLabel: UILabel!
+    @IBOutlet weak var padLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var landingVehicleLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var attemptLabel: UILabel!
+    @IBOutlet weak var successLabel: UILabel!
+    @IBOutlet weak var detailsLabel: UILabel!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
